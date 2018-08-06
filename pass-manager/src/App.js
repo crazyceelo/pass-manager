61d9c853ff62301bc49e483b9d9e48668ff0a5fe
Carlos Perez <solracias@gmail.com>
2018-08-05 22:04:08 -0700

    created a function for a Link to be active. This was custom. Need to do more of this in the future

--- pass-manager/src/App.js
+++ pass-manager/src/App.js
@@ -7,20 +7,30 @@ import { Link, Route } from 'react-router-dom';
 
 // components
 import GamesPage from './components/GamesPage';
+import GameForm from './components/GameForm';
+
+// active link component
+const ActiveLink = ({ label, to, activeOnlyWhenExact }) => (
+  <Route path={to} exact={activeOnlyWhenExact} children={({ match }) => (
+    <Link className={match ? 'active item' : 'item'} to={to}>{label}</Link>
+  )} />
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <Link to="games">Games</Link>
-        </p>
+      <div className="ui container">
+        <div className="ui three item menu">
+          <ActiveLink activeOnlyWhenExact to="/" label="Home" />
+          <ActiveLink activeOnlyWhenExact to="/games" label="Games" />
+          <ActiveLink activeOnlyWhenExact to="/games/new" label="Add New Game" />
+          {/* <Link className="item" to="/">Home</Link>
+          <Link className="item" to="/games">Games</Link>
+          <Link className="item" to="/games/new">Add New Game</Link> */}
+        </div>
 
         <Route exact path="/games" component={GamesPage} />
+        <Route path="/games/new" component={GameForm} />
 
       </div>
     );
---
+++ pass-manager/src/components/GameForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class GameForm extends React.Component {
+  render() {
+    return (
+      <form className="ui form">
+        <h1>Add New Game</h1>
+      </form>
+    )
+  }
+}
+
+export default GameForm;
